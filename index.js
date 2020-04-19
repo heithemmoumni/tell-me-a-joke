7a2c996821a758cca05a96451acc879a64f7c7c8
moumniheithem <moumniheithem@gmail.com>
2020-04-19 21:09:04 +0100

    cleanup + refacto

---
+++ index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+"use strict";
+
+const request = require("request");
+const url = "https://sv443.net/jokeapi/category/programming";
+
+const TYPE_SINGLE = "single";
+const TYPE_TWO_PART = "twopart";
+
+function execute() {
+  request(url, function (error, response, body) {
+    if (error) console.error(error);
+    // && !!body.setup
+    if (response.statusCode === 200) {
+      let response = JSON.parse(body);
+      if (response.type == TYPE_SINGLE) {
+        return console.log(`${response.joke.trim()}`);
+      } else if (response.type == TYPE_TWO_PART) {
+        return console.log(
+          `${response.setup.trim()}\n${response.delivery.trim()}`
+        );
+      }
+    }
+    if (response.statusCode > 200) {
+        return console.error(
+          `Error: ${response.statusCode} - ${response.statusMessage}`
+        );
+      }
+  });
+}
+
+execute();

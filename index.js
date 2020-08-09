fe7082f0c8e1fc893b051feab085515bb9785124
moumniheithem <moumniheithem@gmail.com>
2020-08-09 15:19:16 +0100

    update url joke

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const request = require("request");
 const url = "https://official-joke-api.appspot.com/jokes/random";
 
-const TYPE_SINGLE = "single";
-const TYPE_TWO_PART = "twopart";
-function execute() {
+(function execute() {
   request(url, function (error, response, body) {
     if (error) console.error(error);
 
@@ -11,15 +9,15 @@ function execute() {
       let response = JSON.parse(body);
       return console.log(
         `
+༼∵༽ ༼⍨༽ ༼⍢༽ ༼⍤༽
         \n- ${response.setup.trim()}\n- ${response.punchline.trim()}\n
         `
-      )
+      );
     }
     if (response.statusCode > 200) {
-        return console.error(
-          `Error: ${response.statusCode} - ${response.statusMessage}`
-        );
-      }
+      return console.error(
+        `Error: ${response.statusCode} - ${response.statusMessage}`
+      );
+    }
   });
-}
-execute()
+})()
